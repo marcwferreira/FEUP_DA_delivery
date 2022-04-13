1b3ac8f2c60fe4d6628911e86cb2068eb6947c0d
Marcos Ferreira <72470150+marcwferreira@users.noreply.github.com>
2022-04-13 20:30:55 +0100

    Added case2 to cpp file

--- src/MailSystem.cpp
+++ src/MailSystem.cpp
@@ -140,3 +140,174 @@ void MailSystem::reset()
     this->trucks.sort();
     this->packages.sort();
 }
+
+void MailSystem::case2() {
+
+    this->trucks.sort(byCostAsc); // ascending
+    this->packages.sort(byRewardDesc); 
+
+    list<Package> normalPackages;
+    for(auto elem: packages) {
+        if(!elem.isExpress())
+            normalPackages.push_back(elem);
+    }
+
+    bool statisticVolume = statistics();
+    for (list<Truck>::iterator j = trucks.begin() ; j != trucks.end() ; j++) {
+        bool TruckPositive;
+        if (!normalPackages.empty()) {
+            j->clearPackages();
+            if(statisticVolume)
+                TruckPositive = knapsackVolume( (*j) , normalPackages);
+            else
+                TruckPositive = knapsackWeight((*j) , normalPackages);
+            if (!TruckPositive)
+                break;
+        }
+        else break;
+    }
+
+    int money = 0;
+    for (list<Truck>::iterator j = trucks.begin() ; j != trucks.end() ; j++) {
+        if(j->getEmpty()) continue;
+        int moneyTemp = 0;
+
+        cout << endl << "Carrinha: " << j->getLicencePlate() << endl;
+        cout << "capacidade total (peso): " << j->getMaxWeight() <<" capacidade atigida (peso): " << j->getActualWeight() << endl;
+        cout << "capacidade total (volume): " << j->getMaxVolume() <<" capacidade atigida (volume): " << j->getActualVolume() << endl;
+        if( j->getPackages().empty()) continue;
+        moneyTemp -= j->getCost();
+        cout << "custo dela: " << j->getCost() << endl;
+        for(auto elem: j->getPackages()){
+            moneyTemp += elem.getReward();
+            cout << "Custo do pacote: " << elem.getReward() << " peso do elemento: " << elem.getWeight() << " volume do elemento: " << elem.getVolume() << endl;
+        }
+        cout << "O ganho total foi: " << moneyTemp << endl;
+        money += moneyTemp;
+    }
+
+    cout << endl << "O ganho total foi final: " << money << endl;
+}
+
+bool MailSystem::knapsackWeight(Truck &currentTruck, list<Package> &currentPackages) {
+
+    //table to calculate the reward values
+    std::vector<std::vector<int>> knapsack;
+    knapsack.resize(currentPackages.size()+1);
+    for (unsigned int i = 0; i < currentPackages.size() + 1; i++) {
+        knapsack[i].resize(currentTruck.getMaxWeight() + 1);
+        knapsack[i][0] = 0;
+    }
+    for (unsigned int i = 1; i < currentTruck.getMaxWeight() + 1; i++)
+        knapsack[0][i] = 0;
+
+    list<Package>::iterator pIt = currentPackages.begin();
+    // Computes the maximum value that can be reached varying the number of elements and the capacity
+    for (unsigned int n = 1; n <= currentPackages.size(); n++) {
+        for (unsigned int capacity = 1; capacity <= currentTruck.getMaxWeight(); capacity++) {
+            if ( pIt->getWeight() <= capacity) {
+                if ( ( (int)(( *pIt).getWeight()) + knapsack[n-1][capacity - (int)(( *pIt).getWeight())] ) > knapsack[n-1][capacity])
+                    knapsack[n][capacity] = pIt->getReward()+knapsack[n-1][capacity - pIt->getWeight()];
+                else
+                    knapsack[n][capacity] = knapsack[n - 1][capacity];
+            } 
+        }
+        advance(pIt,1);
+    }
+
+
+    list<list<Package>::iterator> selectedIt; //TEMPORARY - list of iterators just to erase from list
+    int weight = currentTruck.getMaxWeight();
+    int res = knapsack[currentPackages.size()][currentTruck.getMaxWeight()];
+    pIt = currentPackages.end();
+    int packagesGain = 0;
+
+    for( int i = currentPackages.size(); i > 0 && res > 0; i--){
+        --pIt;
+        if ( res == knapsack[i-1][weight])
+            continue;
+        else {
+           if ( (*pIt).getVolume() + currentTruck.getActualVolume() <= currentTruck.getMaxVolume() && (*pIt).getWeight() + currentTruck.getActualWeight() <= currentTruck.getMaxWeight()) {
+                currentTruck.addPackage(*pIt);
+                selectedIt.push_back(pIt);
+                packagesGain += (int)((*pIt).getReward());
+
+                res -= pIt->getReward();
+                weight -= pIt->getWeight();
+            }
+            else 
+                break;
+        }
+    }
+
+    if( (packagesGain- (int)currentTruck.getCost()) >= 0) {
+        for(auto it: selectedIt) currentPackages.erase(it); //TEMPORARY - deleteing items from list
+        return true;
+    }
+    else {
+        currentTruck.clearPackages();
+        return false;
+    }
+
+}
+
+bool MailSystem::knapsackVolume(Truck &currentTruck, list<Package> &currentPackages) {
+
+    //table to calculate the reward values
+    std::vector<std::vector<int>> knapsack;
+    knapsack.resize(currentPackages.size()+1);
+    for (unsigned int i = 0; i < currentPackages.size() + 1; i++) {
+        knapsack[i].resize(currentTruck.getMaxVolume() + 1);
+        knapsack[i][0] = 0;
+    }
+    for (unsigned int i = 1; i < currentTruck.getMaxVolume() + 1; i++)
+        knapsack[0][i] = 0;
+
+    list<Package>::iterator pIt = currentPackages.begin();
+    // Computes the maximum value that can be reached varying the number of elements and the capacity
+    for (unsigned int n = 1; n <= currentPackages.size(); n++) {
+        for (unsigned int capacity = 1; capacity <= currentTruck.getMaxVolume(); capacity++) {
+            if ( pIt->getVolume() <= capacity) {
+                if ( ( (int)(( *pIt).getVolume()) + knapsack[n-1][capacity - (int)(( *pIt).getVolume())] ) > knapsack[n-1][capacity])
+                    knapsack[n][capacity] = pIt->getReward()+knapsack[n-1][capacity - pIt->getVolume()];
+                else
+                    knapsack[n][capacity] = knapsack[n - 1][capacity];
+            } 
+        }
+        advance(pIt,1);
+    }
+
+    list<list<Package>::iterator> selectedIt; //TEMPORARY - list of iterators just to erase from list
+    int volume = currentTruck.getMaxVolume();
+    int res = knapsack[currentPackages.size()][currentTruck.getMaxVolume()];
+    pIt = currentPackages.end();
+    int packagesGain = 0;
+
+    for( int i = currentPackages.size(); i > 0 && res > 0; i--){
+        --pIt;
+        if ( res == knapsack[i-1][volume])
+            continue;
+        else {
+           if ( (*pIt).getVolume() + currentTruck.getActualVolume() <= currentTruck.getMaxVolume() && (*pIt).getWeight() + currentTruck.getActualWeight() <= currentTruck.getMaxWeight()) {
+                currentTruck.addPackage(*pIt);
+                selectedIt.push_back(pIt);
+                packagesGain += (int)((*pIt).getReward());
+
+                res -= pIt->getReward();
+                volume -= pIt->getVolume();
+            }
+            else 
+                break;
+        }
+    }
+
+    if( (packagesGain- (int)currentTruck.getCost()) >= 0) {
+        for(auto it: selectedIt) currentPackages.erase(it); //TEMPORARY - deleteing items from list
+        return true;
+    }
+    else {
+        currentTruck.clearPackages();
+        return false;
+    }
+
+}
